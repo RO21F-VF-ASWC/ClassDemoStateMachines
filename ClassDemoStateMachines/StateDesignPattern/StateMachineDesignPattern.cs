36aa7da7d32ad0547913e6e9ad1f7f6185854277
Peter Levinsky <pele@easj.dk>
2021-03-02 10:29:45 +0100

    Add project files.

---
+++ ClassDemoStateMachines/StateDesignPattern/StateMachineDesignPattern.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDemoStateMachines.StateDesignPattern
+{
+    public class StateMachineDesignPattern:IStateMachine<char>
+    {
+        public bool NextInput(char input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClassDemoStateMachines/StateWorker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ClassDemoStateMachines.StateDesignPattern;
+using ClassDemoStateMachines.StateTable;
+
+namespace ClassDemoStateMachines
+{
+    public class StateWorker
+    {
+
+        public void Start()
+        {
+            Console.Write("Indtast Tekst ");
+            string str = Console.ReadLine();
+
+            bool success = true;
+            IStateMachine<char> sm = new StateMachineTable();
+
+
+            foreach (char c in str)
+            {
+                if (!sm.NextInput(c))
+                {
+                    Console.WriteLine("Fejl i tekst streng er ikke (abc)* - var " + str);
+                    success = false;
+                    break;
+                }
+            }
+
+            if (success)
+            {
+                Console.WriteLine("Tillykke strengen overholdt (abc)* ");
+            }
+        }
+    }
+
+}
